ac68f7a9611a093eda381021607c90a7512b6a1c
LHTHC <starfall1011@gmail.com>
2023-03-22 09:21:03 +0300

    fixed configs

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
-const Main: FC = (props) => {
-  return <div>Main</div>;
+const Main: FC = () => {
+  return <div className="main">Main</div>;
 };
 
 export default Main;
