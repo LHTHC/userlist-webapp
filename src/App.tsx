bc9180b14457f41bc41cefd7d5557c6b91de8f62
LHTHC <starfall1011@gmail.com>
2023-03-22 08:58:22 +0300

    fixed configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import './App.css';
 
-function App() {
-  return <div className='App'>hello</div>;
-}
+const App = () => {
+  return <div className="App">Hello</div>;
+};
 
 export default App;
---
+++ src/Pages/Main.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Main: FC = (props) => {
+  return <div>Main</div>;
+};
+
+export default Main;
