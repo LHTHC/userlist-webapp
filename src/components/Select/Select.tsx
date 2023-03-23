dc6ae900e1bd25bdea5e060bf7c4eff998b1e7e0
LHTHC <starfall1011@gmail.com>
2023-03-23 17:52:11 +0300

    added tooltip animation

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import styles from './Select.module.scss';
 
 type SelectOption = {
@@ -58,33 +59,40 @@ const Select: FC<SelectProps> = ({
       {topDirection && (
         <div className={`${styles.caret} ${isOpen ? styles.open : ''}`} />
       )}
-      <ul
-        className={`${styles.options} ${isOpen ? styles.show : ''} ${
-          topDirection ? styles.top : ''
-        }`}
-      >
-        {value?.value && (
-          <>
-            <li className={`${styles['selected-option']} ${styles.option}`}>
-              {value.value}
-            </li>
-            <div />
-          </>
-        )}
-        {filteredOptions.map((option) => (
-          <li
-            key={option.label}
-            className={styles.option}
-            onClick={(e) => {
-              e.stopPropagation();
-              selectOption(option);
-              setIsOpen(false);
-            }}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.ul
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={`${styles.options} ${isOpen ? styles.show : ''} ${
+              topDirection ? styles.top : ''
+            }`}
           >
-            {option.label}
-          </li>
-        ))}
-      </ul>
+            {value?.value && (
+              <>
+                <li className={`${styles['selected-option']} ${styles.option}`}>
+                  {value.value}
+                </li>
+                <div />
+              </>
+            )}
+            {filteredOptions.map((option) => (
+              <li
+                key={option.label}
+                className={styles.option}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  selectOption(option);
+                  setIsOpen(false);
+                }}
+              >
+                {option.label}
+              </li>
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
